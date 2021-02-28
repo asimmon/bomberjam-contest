6c72df9e085d4b720fd8bdf253c90cc1f83921b8
Anthony Simmon <simmon.anthony@gmail.com>
2021-02-27 23:12:20 -0500

    Removed SQLite

--- website/Bomberjam.Website/Startup.cs
+++ website/Bomberjam.Website/Startup.cs
@@ -1,13 +1,11 @@
 using System;
 using System.IO;
-using System.Text.RegularExpressions;
 using Bomberjam.Website.Authentication;
 using Bomberjam.Website.Database;
 using Bomberjam.Website.Jobs;
 using Bomberjam.Website.Storage;
 using Bomberjam.Website.Utils;
 using Hangfire;
-using Hangfire.Storage.SQLite;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -22,10 +20,6 @@ namespace Bomberjam.Website
 {
     public class Startup
     {
-        private static readonly Regex SqliteDataSourceRegex = new Regex(
-            "Data Source=(?<filename>[a-z0-9]+\\.db)",
-            RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
         public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             this.Configuration = configuration;
@@ -82,7 +76,7 @@ namespace Bomberjam.Website
 
         private void ConfigureDatabase(IServiceCollection services, string dbConnStr) => services.AddDbContext<BomberjamContext>(options =>
         {
-            var dbBuilder = SqliteDataSourceRegex.IsMatch(dbConnStr) ? options.UseSqlite(dbConnStr) : options.UseSqlServer(dbConnStr);
+            var dbBuilder = options.UseSqlServer(dbConnStr);
 
             if (this.Environment.IsDevelopment())
             {
@@ -94,16 +88,10 @@ namespace Bomberjam.Website
         {
             services.AddHangfire(config =>
             {
-                config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170).UseSimpleAssemblyNameTypeSerializer().UseRecommendedSerializerSettings();
-
-                if (SqliteDataSourceRegex.Match(dbConnStr) is { Success: true } match)
-                {
-                    config.UseSQLiteStorage(match.Groups["filename"].Value);
-                }
-                else
-                {
-                    config.UseSqlServerStorage(dbConnStr);
-                }
+                config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseRecommendedSerializerSettings()
+                    .UseSqlServerStorage(dbConnStr);
             });
 
             services.AddHangfireServer();
