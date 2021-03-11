2380ab5959e340cf6e2e43837ac7ebc6d702e349
Anthony Simmon <simmon.anthony@gmail.com>
2021-03-10 20:33:11 -0500

    Better file streaming over http in worker

--- worker/backend.py
+++ worker/backend.py
@@ -1,7 +1,8 @@
 import json
 import logging
-import requests
 import os
+import requests
+import shutil
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 import util
@@ -18,40 +19,39 @@ def download_bot(bot_id, storage_dir, is_compiled=False):
     """Downloads and stores a bot's zip file locally"""
     logging.debug(f"Downloading {('compiled' if is_compiled else 'source code')} bot {bot_id}...")
 
-    request_url = API_BASE_URL + (f"bot/%s/download?compiled=%s" % (bot_id, "true" if is_compiled else "false"))
-    request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
-
-    r = requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL)
-    r.raise_for_status()
-
     local_file_name = "bot-%s-%s.zip" % (bot_id, "1" if is_compiled else "0")
     local_file_path = os.path.join(storage_dir, local_file_name)
 
-    if os.path.exists(local_file_path):
-        os.remove(local_file_path)
+    request_url = API_BASE_URL + (f"bot/%s/download?compiled=%s" % (bot_id, "true" if is_compiled else "false"))
+    request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
 
-    with open(local_file_path, "wb") as file:
-        file.write(r.content)
+    with requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL, stream=True) as r:
+        r.raw.decode_content = True
+        r.raise_for_status()
 
-    logging.debug(f"Downloaded %s bytes" % len(r.content))
+        if os.path.exists(local_file_path):
+            os.remove(local_file_path)
+
+        with open(local_file_path, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
+
+    logging.debug(f"Downloaded {os.path.getsize(local_file_path)} bytes on disk")
 
     return local_file_path
 
 
 def upload_bot(bot_id, zip_file_path):
-    """Posts a bot file to the manager"""
-    logging.debug(f"Uploading compiled bot {bot_id}...")
-
-    with open(zip_file_path, "rb") as f:
-        file_bytes = f.read()
+    """Posts a bot file to the website"""
+    logging.debug(f"Uploading compiled bot {bot_id} of size {os.path.getsize(zip_file_path)} bytes on disk...")
 
     request_url = API_BASE_URL + ("bot/%s/upload" % bot_id)
     request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
 
-    r = requests.post(request_url, data=file_bytes, headers=request_headers, verify=API_VERIFY_SSL)
-    r.raise_for_status()
+    with open(zip_file_path, "rb") as f:
+        with requests.post(request_url, data=f, headers=request_headers, verify=API_VERIFY_SSL) as r:
+            r.raise_for_status()
 
-    logging.debug(f"Uploaded %s bytes" % len(file_bytes))
+    logging.debug("Uploaded")
 
 
 def send_compilation_result(bot_id, did_compile, language, compilation_errors):
@@ -63,14 +63,15 @@ def send_compilation_result(bot_id, did_compile, language, compilation_errors):
         'Authorization': 'Secret ' + API_AUTH_TOKEN,
         'Content-Type': 'application/json'
     }
-    r = requests.post(request_url, headers=request_headers, verify=API_VERIFY_SSL, json={
+    request_data = {
         'botId': bot_id,
         'didCompile': True if did_compile else False,
         'language': language,
         'errors': compilation_errors
-    })
-    r.raise_for_status()
-    logging.debug("Sent compilation result: %s" % r.text)
+    }
+    with requests.post(request_url, headers=request_headers, verify=API_VERIFY_SSL, json=request_data) as r:
+        r.raise_for_status()
+        logging.debug("Sent compilation result: %s" % r.text)
 
 
 def get_player_compiled_bot_id(player_id):
@@ -79,10 +80,10 @@ def get_player_compiled_bot_id(player_id):
     request_url = API_BASE_URL + ("user/%s/bot" % player_id)
     request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
 
-    r = requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL)
-    r.raise_for_status()
+    with requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL) as r:
+        r.raise_for_status()
+        bot_id = r.text
 
-    bot_id = r.text
     logging.info("Latest compiled bot ID for player %s: %s" % (player_id, bot_id))
     return bot_id
 
@@ -95,15 +96,16 @@ def send_game_result(game):
         'Authorization': 'Secret ' + API_AUTH_TOKEN,
         'Content-Type': 'application/json'
     }
-    r = requests.post(request_url, headers=request_headers, verify=API_VERIFY_SSL, json={
+    request_data = {
         'serializedHistory': game.game_result,
         'standardOutput': game.game_stdout,
         'standardError': game.game_stderr,
         'playerBotIds': game.players_bot_ids,
         'origin': game.origin
-    })
-    r.raise_for_status()
-    logging.debug("Sent game result: %s" % r.text)
+    }
+    with requests.post(request_url, headers=request_headers, verify=API_VERIFY_SSL, json=request_data) as r:
+        r.raise_for_status()
+        logging.debug("Sent game result: %s" % r.text)
 
 
 def get_next_task():
@@ -113,16 +115,14 @@ def get_next_task():
     request_url = API_BASE_URL + 'task/next'
     request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
 
-    r = requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL)
+    with requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL) as r:
+        if r.status_code == 404:
+            return None
 
-    if r.status_code == 404:
-        return None
-
-    r.raise_for_status()
-
-    task = json.loads(r.text)
-    logging.info("Next task ID: %s, type: %s" % (task['id'], task['type']))
-    return task
+        r.raise_for_status()
+        task = json.loads(r.text)
+        logging.info("Next task ID: %s, type: %s" % (task['id'], task['type']))
+        return task
 
 
 def mark_task_started(task_id):
@@ -131,8 +131,8 @@ def mark_task_started(task_id):
     request_url = API_BASE_URL + ("task/%s/started" % task_id)
     request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
 
-    r = requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL)
-    r.raise_for_status()
+    with requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL) as r:
+        r.raise_for_status()
 
     logging.info(f"Marked task: {task_id} as started")
 
@@ -143,7 +143,7 @@ def mark_task_finished(task_id):
     request_url = API_BASE_URL + ("task/%s/finished" % task_id)
     request_headers = {'Authorization': 'Secret ' + API_AUTH_TOKEN}
 
-    r = requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL)
-    r.raise_for_status()
+    with requests.get(request_url, headers=request_headers, verify=API_VERIFY_SSL) as r:
+        r.raise_for_status()
 
     logging.info(f"Marked task: {task_id} as finished")
