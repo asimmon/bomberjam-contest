092d3fd573bcc4ed475a2f4422a6039d42951ee8
Anthony Simmon <anthony.simmon@gsoft.com>
2021-04-19 11:38:56 -0400

    Escaping full path of C# starter bot with double quotes

--- starters/CSharp/run_game.bat
+++ starters/CSharp/run_game.bat
@@ -1,6 +1,6 @@
 dotnet build
 ..\..\engine\bomberjam.exe --output replay.json ^
-    "dotnet %cd%\bin\Debug\netcoreapp3.1\MyBot.dll --logging" ^
-    "dotnet %cd%\bin\Debug\netcoreapp3.1\MyBot.dll" ^
-    "dotnet %cd%\bin\Debug\netcoreapp3.1\MyBot.dll" ^
-    "dotnet %cd%\bin\Debug\netcoreapp3.1\MyBot.dll"
+    "dotnet ""%cd%\bin\Debug\netcoreapp3.1\MyBot.dll"" --logging" ^
+    "dotnet ""%cd%\bin\Debug\netcoreapp3.1\MyBot.dll""" ^
+    "dotnet ""%cd%\bin\Debug\netcoreapp3.1\MyBot.dll""" ^
+    "dotnet ""%cd%\bin\Debug\netcoreapp3.1\MyBot.dll"""
