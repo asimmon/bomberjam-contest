eea553feb9c9be5393f1714829d29800c52a2ce3
Anthony Simmon <simmon.anthony@gmail.com>
2021-11-26 13:28:31 -0500

    Smaller bundle size in development

--- website/Bomberjam.Website/Frontend/scripts/fontawesome.ts
+++ website/Bomberjam.Website/Frontend/scripts/fontawesome.ts
@@ -1,6 +1,13 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faPlay, faPause, faVideo, faExternalLinkAlt, faDownload, faBomb } from '@fortawesome/free-solid-svg-icons';
-import { faWindows, faLinux, faApple } from '@fortawesome/free-brands-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons/faPlay';
+import {  faPause } from '@fortawesome/free-solid-svg-icons/faPause';
+import { faVideo } from '@fortawesome/free-solid-svg-icons/faVideo';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt';
+import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload';
+import { faBomb } from '@fortawesome/free-solid-svg-icons/faBomb';
+import { faWindows } from '@fortawesome/free-brands-svg-icons/faWindows';
+import { faLinux } from '@fortawesome/free-brands-svg-icons/faLinux';
+import { faApple } from '@fortawesome/free-brands-svg-icons/faApple';
 
 library.add(faPlay, faPause, faVideo, faExternalLinkAlt, faDownload, faBomb, faWindows, faLinux, faApple);
 dom.watch();
