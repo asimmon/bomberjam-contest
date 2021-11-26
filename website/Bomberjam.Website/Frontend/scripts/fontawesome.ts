97a0985c873fe9cd825b1660ce3e2093ebe5195b
Anthony Simmon <simmon.anthony@gmail.com>
2021-11-26 12:04:55 -0500

    Using FA brand icons

--- website/Bomberjam.Website/Frontend/scripts/fontawesome.ts
+++ website/Bomberjam.Website/Frontend/scripts/fontawesome.ts
@@ -1,5 +1,6 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faPlay, faPause, faVideo, faExternalLinkAlt, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faWindows, faLinux, faApple } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faPlay, faPause, faVideo, faExternalLinkAlt, faDownload);
+library.add(faPlay, faPause, faVideo, faExternalLinkAlt, faDownload, faWindows, faLinux, faApple);
 dom.watch();
