ef545375c2635521c3bf1830d9468be1d1267d08
Anthony Simmon <simmon.anthony@gmail.com>
2021-11-26 12:18:41 -0500

    Added bomb icon in logo

--- website/Bomberjam.Website/Frontend/scripts/fontawesome.ts
+++ website/Bomberjam.Website/Frontend/scripts/fontawesome.ts
@@ -1,6 +1,6 @@
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faPlay, faPause, faVideo, faExternalLinkAlt, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPause, faVideo, faExternalLinkAlt, faDownload, faBomb } from '@fortawesome/free-solid-svg-icons';
 import { faWindows, faLinux, faApple } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faPlay, faPause, faVideo, faExternalLinkAlt, faDownload, faWindows, faLinux, faApple);
+library.add(faPlay, faPause, faVideo, faExternalLinkAlt, faDownload, faBomb, faWindows, faLinux, faApple);
 dom.watch();
