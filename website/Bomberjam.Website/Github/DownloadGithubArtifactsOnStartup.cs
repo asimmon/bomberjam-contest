83f8ec2a6e32e9680995d9920e6ae3f3dac3c8cb
Anthony Simmon <simmon.anthony@gmail.com>
2022-05-01 11:01:53 -0400

    Prevent github artifact sync from crashing the website

--- website/Bomberjam.Website/Github/DownloadGithubArtifactsOnStartup.cs
+++ website/Bomberjam.Website/Github/DownloadGithubArtifactsOnStartup.cs
@@ -1,21 +1,32 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Bomberjam.Website.Github
 {
     public sealed class DownloadGithubArtifactsOnStartup : BackgroundService
     {
         private readonly IGithubArtifactManager _downloader;
+        private readonly ILogger _logger;
 
-        public DownloadGithubArtifactsOnStartup(IGithubArtifactManager downloader)
+        public DownloadGithubArtifactsOnStartup(IGithubArtifactManager downloader, ILogger<DownloadGithubArtifactsOnStartup> logger)
         {
             this._downloader = downloader;
+            this._logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await this._downloader.Initialize(stoppingToken);
+            try
+            {
+                await this._downloader.Initialize(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError(ex, "An error occurred while downloading latest github artifacts");
+            }
         }
     }
 }
